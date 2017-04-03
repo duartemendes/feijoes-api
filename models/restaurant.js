ca0f45d1ff9d6b21771bfb07f061fc4f5ed0dca0
Duarte Mendes <duartecostamendes@gmail.com>
2017-04-03 12:32:08 +0100

    Improve Restaurant and User models

--- models/restaurant.js
+++ models/restaurant.js
@@ -6,12 +6,14 @@ const restaurantSchema = mongoose.Schema({
     unique: true,
     required: true
   },
+  favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   dishes: [{
     _id: { id: false },
     // TODO: create Dish Schema
     // dish: { type: mongoose.Schema.Types.ObjectId, ref: 'Dish' },
     dishName: String,
     date: { type: Date, default: Date.now },
+    creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     images: [String],
     prices: {
       single: Number,
@@ -30,7 +32,21 @@ const restaurantSchema = mongoose.Schema({
       up: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
       down: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
     }
-  }]
+  }],
+  defaultPrice: {
+    single: Number,
+    menu: {
+      price: Number,
+      includes: {
+        bread: { type: Boolean, default: false },
+        soup: { type: Boolean, default: false },
+        drink: { type: Boolean, default: false },
+        coffee: { type: Boolean, default: false },
+        dessert: { type: Boolean, default: false }
+      }
+    }
+  },
+  totalValidDishes: { type: Number, default: 0 }
 })
 
 module.exports = mongoose.model('Restaurant', restaurantSchema)
--- models/user.js
+++ models/user.js
@@ -8,7 +8,14 @@ const userSchema = mongoose.Schema({
   },
   tokenFB: {
     type: String
-  }
+  },
+  favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Restaurant' }],
+  score: { type: Number, default: 0 },
+  votes: {
+    up: { type: Number, default: 0 },
+    down: { type: Number, default: 0 }
+  },
+  dishesSubmitted: { type: Number, default: 0 }
 })
 
 module.exports = mongoose.model('User', userSchema)
